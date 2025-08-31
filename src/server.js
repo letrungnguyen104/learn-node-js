fa62af4317db166c29e48ddd06df877e2d8a0973
Le Trung Nguyen <letrungnguyen1004@gmail.com>
2025-08-31 11:52:31 +0700

    learn-env-static

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+require('dotenv').config();
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOSTNAME;
 
 //config template engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//config static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Khai báo route
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World with Nodemon!');
 })
 
 app.get('/abc', (req, res) => {
