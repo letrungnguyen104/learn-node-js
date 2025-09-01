2cdb595be170fc6211542500875e09d81022f1ff
Le Trung Nguyen <letrungnguyen1004@gmail.com>
2025-09-01 18:35:00 +0700

    MVC Design Pattern

--- src/server.js
+++ src/server.js
@@ -1,30 +1,17 @@
-const express = require('express');
-const path = require('path');
-const app = express();
 require('dotenv').config();
+const express = require('express');
+const configViewEngine = require('./config/viewEngine');
+const webRoutes = require('./routes/web');
+const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOSTNAME;
 
 //config template engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+configViewEngine(app);
 
-//config static files
-app.use(express.static(path.join(__dirname, 'public')))
+//khai báo route
+app.use('/test', webRoutes);
 
-//Khai báo route
-app.get('/', (req, res) => {
-    res.send('Hello World with Nodemon!');
-})
-
-app.get('/abc', (req, res) => {
-    res.send('Check ABC!');
-})
-
-app.get('/test', (req, res) => {
-    res.render('sample.ejs');
-})
-
-app.listen(port, () => {
+app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`);
 })
