cdf8c99308ff8081448a66dd245db6b0ccb049c7
letrungnguyen104 <letrungnguyen1004@gmail.com>
2025-09-05 13:25:30 +0700

    Done Create User

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { getHomepage, getImage } = require('../controllers/homeController');
+const { getHomepage, getImage, postCreateUser } = require('../controllers/homeController');
 const router = express.Router();
 
 //Khai báo route
 router.get('/', getHomepage)
 
 router.get('/image', getImage)
+
+router.post('/create-user', postCreateUser);
 
 module.exports = router;
